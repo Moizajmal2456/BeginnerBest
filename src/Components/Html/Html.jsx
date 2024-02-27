afeb94596e05e1cedae078c7f23ccfdf14b52be8
Moizajmal2456 <moizajmal2456@gmail.com>
2024-02-27 17:02:27 -0500

    Changes in html page

--- src/Components/Html/Html.jsx
+++ src/Components/Html/Html.jsx
@@ -12,7 +12,14 @@ return(
       ))}
    </div>
    <div className={style.Detail}>
-
+    <h2>HTML</h2>
+    <p>
+      HTML (Hypertext Markup Language) is the standard markup language used to create and design documents on the World Wide Web.
+      It forms the backbone of web content, providing a structure for organizing and presenting information on websites. HTML uses a system
+      of tags to define various elements within a document, and these tags are interpreted by web browsers to render the content as intended.
+    </p>
+    <h2>Document Structure</h2>
+    <p></p>
    </div>
  </div>
 );
